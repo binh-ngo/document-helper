5727ed5d5e26b635e035b556476236b12c4d39cc
Binh Ngo <bnngo97@gmail.com>
2024-02-04 11:09:20 -0800

    implemented chat history in streamlit session state to connect subsequent prompts

--- backend/core.py
+++ backend/core.py
@@ -1,12 +1,12 @@
 import os
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains import ConversationalRetrievalChain
 from langchain_community.vectorstores import Pinecone
 import pinecone
 from dotenv import load_dotenv
 from consts import INDEX_NAME
-from typing import Any
+from typing import Any, List, Dict
 
 load_dotenv()
 # Initialize pinecone client
@@ -16,19 +16,20 @@ pinecone.init(
 )
 
 
-def run_llm(query: str) -> Any:
+def run_llm(query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
     embeddings = OpenAIEmbeddings()
     docsearch = Pinecone.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
     chat = ChatOpenAI(verbose=True, temperature=0)
-    qa = RetrievalQA.from_chain_type(
+    # RetrievalQA module runs from_chain_type function but ConversationalRetrievalChain doesn't have that function
+    qa = ConversationalRetrievalChain.from_llm(
         llm=chat,
         chain_type="stuff",
         retriever=docsearch.as_retriever(),
         return_source_documents=True,
     )
-    return qa({"query": query})
+    return qa({"question": query, "chat_history": chat_history})
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -8,10 +8,14 @@ streamlit.header("Documentation Helper Bot")
 prompt = streamlit.text_input("Prompt", placeholder="Enter your prompt here...")
 
 if "user_prompt_history" not in streamlit.session_state:
-  streamlit.session_state["user_prompt_history"] = []
+    streamlit.session_state["user_prompt_history"] = []
 
 if "chat_answers_history" not in streamlit.session_state:
     streamlit.session_state["chat_answers_history"] = []
+
+if "chat_history" not in streamlit.session_state:
+    streamlit.session_state["chat_history"] = []
+
 
 def create_sources_string(source_urls: Set[str]) -> str:
     if not source_urls:
@@ -26,19 +30,27 @@ def create_sources_string(source_urls: Set[str]) -> str:
 
 if prompt:
     with streamlit.spinner("Generating response..."):
-        generated_response = run_llm(query=prompt)
+        generated_response = run_llm(
+            query=prompt, chat_history=streamlit.session_state["chat_history"]
+        )
         sources = set(
             [doc.metadata["source"] for doc in generated_response["source_documents"]]
         )
 
         formatted_response = (
-            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
         )
 
         streamlit.session_state["user_prompt_history"].append(prompt)
         streamlit.session_state["chat_answers_history"].append(formatted_response)
+        streamlit.session_state["chat_history"].append((
+            prompt, generated_response["answer"]
+        ))
 
 if streamlit.session_state["chat_answers_history"]:
-    for generated_response, user_query in zip(streamlit.session_state["chat_answers_history"], streamlit.session_state["user_prompt_history"]):
+    for generated_response, user_query in zip(
+        streamlit.session_state["chat_answers_history"],
+        streamlit.session_state["user_prompt_history"],
+    ):
         message(user_query, is_user=True)
         message(generated_response)
