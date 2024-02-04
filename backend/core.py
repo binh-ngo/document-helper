be7efc94bd26b068fa3474d849c59dad9f6dd5de
Binh Ngo <bnngo97@gmail.com>
2024-02-03 21:24:08 -0800

    implemented retrieval qa chain

---
+++ backend/core.py
@@ -0,0 +1,33 @@
+import os
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain_community.vectorstores import Pinecone
+import pinecone
+from dotenv import load_dotenv
+from consts import INDEX_NAME
+from typing import Any
+load_dotenv()
+# Initialize pinecone client
+pinecone.init(
+    api_key=os.environ["PINECONE_API_KEY"],
+    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+)
+
+
+def run_llm(query: str) -> Any:
+    embeddings = OpenAIEmbeddings()
+    docsearch = Pinecone.from_existing_index(
+        index_name=INDEX_NAME, embedding=embeddings
+    )
+    chat = ChatOpenAI(verbose=True, temperature=0)
+    qa = RetrievalQA.from_chain_type(
+        llm=chat,
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+        return_source_documents=True,
+    )
+    return qa({"query": query})
+
+if __name__ == "__main__":
+  print(run_llm(query="What is Langchain?"))
--- ingestion.py
+++ ingestion.py
@@ -5,9 +5,10 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Pinecone
 import pinecone
 from dotenv import load_dotenv
+from consts import INDEX_NAME
 
 load_dotenv()
-# Initialize pinecone client 
+# Initialize pinecone client
 pinecone.init(
     api_key=os.environ["PINECONE_API_KEY"],
     environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
@@ -19,25 +20,23 @@ def ingest_docs() -> None:
     loader = ReadTheDocsLoader(path="langchain-docs/langchain.readthedocs.io/en/latest")
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents)} documents")
-    # Chunks information from all documents 
+    # Chunks information from all documents
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
     )
     documents = text_splitter.split_documents(documents=raw_documents)
     print(f"Splitted into {len(documents)} chunks")
 
-# changes the source from the file path to the url
+    # changes the source from the file path to the url
     for doc in documents:
         old_path = doc.metadata["source"]
         new_url = old_path.replace("langchain-docs", "https:/")
         doc.metadata.update({"source": new_url})
 
-# takes the chunked info and inserts them into Pinecone
+    # takes the chunked info and inserts them into Pinecone
     print(f"Going to insert {len(documents)} to Pinecone")
     embeddings = OpenAIEmbeddings()
-    Pinecone.from_documents(
-        documents, embeddings, index_name="langchain-doc-index"
-    )
+    Pinecone.from_documents(documents, embeddings, index_name=INDEX_NAME)
     print("****** Added to Pinecone vectorstore vectors")
 
 
